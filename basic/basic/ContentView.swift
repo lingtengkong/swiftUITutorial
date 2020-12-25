3323b8dc652510aa105726a57ded74c771de8b44
lingtengkong <jn19830@bristol.ac.uk>
2020-12-25 23:39:22 +0000

    basic

---
+++ basic/basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basic
+//
+//  Created by Handsome Ling on 25/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ basic/basic/basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicApp.swift
+//  basic
+//
+//  Created by Handsome Ling on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
